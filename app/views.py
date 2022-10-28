4740910186e9a30faf54cb4be470af94c7af4d01
harshadvali11 <harshadvali1431@gmail.com>
2022-10-28 18:59:24 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -18,6 +18,54 @@ def insert_topic(request):
         return HttpResponse('Topic is inserted successfully go and check in admin if u want')
     return render(request,'insert_topic.html')
 
+def insert_webpage(request):
+    topics=Topic.objects.all()
+    d={'topics':topics}
+    if request.method=='POST':
+        tn=request.POST.get('topic')
+        n=request.POST.get('name')
+        u=request.POST.get('url')
+        T=Topic.objects.get_or_create(topic_name=tn)[0]
+        T.save()
+        W=Webpage.objects.get_or_create(topic_name=T,name=n,url=u)[0]
+        W.save()
+        return HttpResponse('Webpage is inserted successfully')
+    return render(request,'insert_webpage.html',d)
+
+def select_topic(request):
+    topics=Topic.objects.all()
+    d={'topics':topics}
+    if request.method=='POST':
+        tn=request.POST.getlist('topic')
+        print(tn)
+        webpages=Webpage.objects.none()
+        for i in tn:
+            webpages=webpages | Webpage.objects.filter(topic_name=i)
+        d1={'webpages':webpages}
+        return render(request,'display_webpage.html',d1)
+    return render(request,'select_topic.html',d)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     
--- project3/urls.py
+++ project3/urls.py
@@ -20,6 +20,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('htmlforms/',htmlforms,name='htmlforms'),
     path('insert_topic/',insert_topic,name='insert_topic'),
+    path('insert_webpage/',insert_webpage,name='insert_webpage'),
+    path('select_topic/',select_topic,name='select_topic'),
 ]
 
 
